198d3117f3ac21db52cdc9fffccdcb4cd3e9bbd9
nuanxinqing <nuanxinqing@gmail.com>
2022-01-28 23:43:56 +0800

    Update

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import re
 from pyselenium import QRSelenium as qps
 from flask import Flask, render_template, jsonify, request
 from qinglong import addCookie
-from pyselenium import codeSelenium as cps
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -24,74 +23,6 @@ def index():
 
     Title = config_json["Main"]["Title"]
     return render_template("index.html", Title=Title)
-
-
-@app.route('/NumberLogin')
-def numLogin():
-    with open("./config/config.json", encoding="utf-8") as f:
-        config_json = json.load(f)
-
-    Title = config_json["Main"]["Title"]
-    return render_template("numLogin.html", Title=Title)
-
-
-@app.route("/api/LoginNum", methods=['POST'])
-def loginnum():
-    data = request.get_json(force=True)
-    number = data.get("number")
-    print(number)
-    if number != "":
-        ret = re.match(r"^1[356789]\d{9}$", str(number))
-        if ret:
-            res = cps.CodeStart(number)
-            if res == 1006:
-                return jsonify(code=1001, msg="已尝试自动通过滑块验证，如果长时间没收到验证码，请重新登录")
-            elif res == 1003:
-                return jsonify(code=1002, msg="滑块验证失败，请稍等十分钟后重试")
-            else:
-                return jsonify(code=1001, msg="请输入6位验证码")
-        else:
-            return jsonify(code=1002, msg="请输入正确的手机号码")
-    else:
-        return jsonify(code=1002, msg="参数不完整")
-
-
-@app.route("/api/LoginCode", methods=['POST'])
-def logincode():
-    data = request.get_json(force=True)
-    code = data.get("code")
-    if len(code) == 6:
-        num = cps.CodeStart2(code)
-        if num == 1002:
-            return jsonify(code=1002, msg="Cookie获取失败，请重新获取")
-        elif num == 1005:
-            # 上传Cookie
-            addCookie.add_Cookie(num)
-            return jsonify(code=1005, msg="需要身份验证")
-        else:
-            return jsonify(code=1001, msg="Cookie获取成功", ck=num)
-    else:
-        return jsonify(code=1002, msg="请输入正确的验证码")
-
-
-@app.route("/api/idCode", methods=['POST'])
-def idCode():
-    data = request.get_json(force=True)
-    code = data.get("idCode")
-    if len(str(code)) != 6:
-        return jsonify(code=1002, msg="请输入六位身份码")
-    else:
-        idList = []
-        strCode = str(code)
-        for i in strCode:
-            idList.append(str(i))
-
-        num = cps.CodeStart3(idList)
-
-        if num == 1002:
-            return jsonify(code=1002, msg="获取错误")
-        else:
-            return jsonify(code=1001, msg="Cookie获取成功", ck=num)
 
 
 @app.route('/QRLogin')
